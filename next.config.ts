f5a9f9f55daf4f6a5367f20f3623b711c2a67d36
Sobi Aminnejad <s.aminnejad2013@gmail.com>
2025-05-11 12:02:56 +0200

    update nextjs config to fix image rendering on github page

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,16 @@
 import type { NextConfig } from "next";
+const isProd = process.env.NODE_ENV === "production";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  // Tell Next.js all pages and links live under /portfolio
+  basePath: isProd ? "/portfolio" : "",
+  // Prefix all static assets (images, JS, CSS) with /portfolio/
+  assetPrefix: isProd ? "/portfolio/" : "",
+  // Disable image optimization for static export
+  images: {
+    unoptimized: true,
+  },
+  output: "export",
 };
 
 export default nextConfig;
